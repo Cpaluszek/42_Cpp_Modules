d87fdb5add8598912c93b710e65b4354d73b32ff
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-03-15 15:21:55 +0100

    remove debug prints

--- Cpp02/ex02/Fixed.cpp
+++ Cpp02/ex02/Fixed.cpp
@@ -14,37 +14,28 @@
 
 const int Fixed::_fractionalBits = 8;
 
-Fixed::Fixed() : _value(0) {
-	std::cout << "Default constructor called" << std::endl;
-}
+Fixed::Fixed() : _value(0) {}
 
 Fixed::Fixed(const int n) {
-	std::cout << "Int constructor called" << std::endl;
 	this->_value = (n << Fixed::_fractionalBits);
 }
 
 Fixed::Fixed(const float n) {
-	std::cout << "Float constructor called" << std::endl;
 	this->_value = static_cast<int>(roundf(n * (1 << Fixed::_fractionalBits)));
 }
 
 Fixed::Fixed(const Fixed &src) {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 }
 
-Fixed::~Fixed() {
-	std::cout << "Destructor called" << std::endl;
-}
+Fixed::~Fixed() {}
 
 Fixed & Fixed::operator=(const Fixed &rhs) {
-	std::cout << "Copy assignment operator called" << std::endl;
 	this->_value = rhs.getRawBits();
 	return *this;
 }
 
 int Fixed::getRawBits() const {
-	std::cout << "getRawBits member function called" << std::endl;
 	return this->_value;
 }
 
