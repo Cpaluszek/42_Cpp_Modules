804b7e0fa97d9a880464c0f057e960e6d07ce892
Clement Paluszek <cpalusze@z4r10p1.42lyon.fr>
2023-03-24 13:23:09 +0100

    fix leaks

--- Cpp04/ex03/main.cpp
+++ Cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 11:49:49 by cpalusze          #+#    #+#             */
-/*   Updated: 2023/03/20 13:39:22 by cpalusze         ###   ########.fr       */
+/*   Updated: 2023/03/24 12:45:32 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int main() {
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
+	delete tmp;
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
 	ICharacter* bob = new Character("bob");
@@ -34,5 +35,6 @@ int main() {
 	delete bob;
 	delete me;
 	delete src;
+	delete tmp;
 	return 0;
 }
